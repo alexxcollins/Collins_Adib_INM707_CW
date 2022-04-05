2687a190f6430a7c81a4ab3b0e08b99b005c6521
Alex Collins <alexxcollins@googlemail.com>
2022-04-05 12:21:02 +0200

    added moving average options to visualisations

--- RL_utils.py
+++ RL_utils.py
@@ -37,25 +37,54 @@ def calculate_rewards(R_matrix, path):
 def path_in_grids(path, dims):
     return [(cell//dims[1], cell%dims[0]) for cell in path]
 
+def moving_average(a, n=3) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
 
-def average_R_display(Rtot):
-    Rtot_avg = Rtot.cumsum() / np.arange(1, len(Rtot) + 1)
-    plt.plot(np.arange(20, len(Rtot_avg)), Rtot_avg[20:])
+def average_R_display(Rtot, rolling_periods=None):
+    """display average of total rewards over episodes
+    
+    rolling_periods: None or int
+        if None, display the cumulative average.
+        if int, display the moving average over n episodes
+            this is more helpful for seeing convergence
+            as earlier very large/small R-values are discarded.
+    """
+    if rolling_periods:
+        Rtot_avg = moving_average(Rtot, n=rolling_periods)
+        x = np.arange(rolling_periods - 1, len(Rtot))
+    else:
+        Rtot_avg = (Rtot.cumsum() / np.arange(1, len(Rtot) + 1))[20:]
+        x = np.arange(20, len(Rtot))
+        
+    plt.plot(x, Rtot_avg)
     plt.title("Average Rewards per episode")
     plt.show()
 
-
-def steps_plot(a_hist):
+def steps_plot(a_hist, rolling_periods=None):
+    """
+    rolling_periods: None or int
+        if None, display the cumulative average.
+        if int, display the moving average over n episodes
+            this is more helpful for seeing convergence
+            as earlier very large/small R-values are discarded.
+    """
     y = [len(h) for h in a_hist]
     x = np.arange(len(a_hist))
 
-    y_avg = np.cumsum(y) / np.arange(1, len(y) + 1)
-    plt.plot(np.arange(20, len(y_avg)), y_avg[20:])
+    if rolling_periods:
+        y_avg = moving_average(y, n=rolling_periods)
+        x = np.arange(rolling_periods - 1, len(y))
+    else:
+        y_avg = (np.cumsum(y) / np.arange(1, len(y) + 1))[20:]
+        x = np.arange(20, len(y))
+    plt.plot(x, y_avg)
     plt.title("Average steps per episode")
     plt.show()
 
 
-def plot_Q_changing(Q_hist, absolute=True):
+def plot_Q_changing(Q_hist, absolute=True, rolling_periods=None):
     
     if absolute:
         difference_Qs = np.absolute(Q_hist[1:,:,:] - Q_hist[:-1, :, :])
@@ -63,7 +92,12 @@ def plot_Q_changing(Q_hist, absolute=True):
         difference_Qs = Q_hist[1:,:,:] - Q_hist[:-1, :, :]
 
     Q_plot = np.sum(difference_Qs, axis=(1,2))
+    if rolling_periods:
+        x = np.arange(rolling_periods - 1, len(Q_plot))
+        Q_plot = moving_average(Q_plot, n=rolling_periods)
+    else:
+        x = np.arange(len(Q_plot))
 
-    plt.plot(np.arange(len(Q_plot)), Q_plot)
+    plt.plot(x, Q_plot)
     plt.title("Changing in Q matrixs")
     plt.show()
